0ae170a8e86cdc7b1f8f3fda8aca8eeea0771d6b
inaz2 <inaz2@twitter>
2015-03-13 11:07:49 +0900

    converter(): add bin/oct/dec/hex

--- test.sh
+++ test.sh
@@ -26,12 +26,16 @@ test 'use just as cat' 'yes | head | ./dog -n' '     1	y
      9	y
     10	y'
 
-test 'convert as rot13' 'echo -n 1234TESTtest | ./dog -c rot13' '1234GRFGgrfg'
-test 'convert as upper' 'echo -n 1234TESTtest | ./dog -c upper' '1234TESTTEST'
-test 'convert as lower' 'echo -n 1234TESTtest | ./dog -c lower' '1234testtest'
+test 'convert as bin' 'echo -n 0o52 | ./dog -c bin' '0b101010'
+test 'convert as oct' 'echo -n 42 | ./dog -c oct' '0o52'
+test 'convert as dec' 'echo -n 0x2a | ./dog -c dec' '42'
+test 'convert as hex' 'echo -n 0b101010 | ./dog -c hex' '0x2a'
 test 'convert as crlf' 'echo -en "/\r\n/\r/\n/" | ./dog -c crlf' $'/\r\n/\r\n/\r\n/'
 test 'convert as cr' 'echo -en "/\r\n/\r/\n/" | ./dog -c cr' $'/\r/\r/\r/'
 test 'convert as lf' 'echo -en "/\r\n/\r/\n/" | ./dog -c lf' $'/\n/\n/\n/'
+test 'convert as upper' 'echo -n 1234TESTtest | ./dog -c upper' '1234TESTTEST'
+test 'convert as lower' 'echo -n 1234TESTtest | ./dog -c lower' '1234testtest'
+test 'convert as rot13' 'echo -n 1234TESTtest | ./dog -c rot13' '1234GRFGgrfg'
 
 test 'encode as hex' 'echo -n 日本語 | ./dog -e hex | base64' 'XHhlNlx4OTdceGE1XHhlNlx4OWNceGFjXHhlOFx4YWFceDll'
 test 'encode as unicode' 'echo -n 日本語 | ./dog -e unicode | base64' 'XHU2NWU1XHU2NzJjXHU4YTll'
